eece4a4ce13d3ec26e94bf23fe5a9c7e4b32a20f
oliverjam <oliverjamesphillips@gmail.com>
2025-03-21 11:59:30 +0000

    Add oli solution

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 class Bowling {
-  constructor() {}
+	constructor() {
+		this.frameIndex = 0;
+		this.frames = Array.from({ length: 10 }, () => []);
+	}
 
-  roll(pins) {}
+	roll(pins) {
+		const frame = this.frames[this.frameIndex];
+		frame.push(pins);
 
-  getScore() {}
+		const frameFinished = frame.length === 2;
+		if (frameFinished) {
+			this.frameIndex += 1;
+		}
+	}
+
+	getScore() {
+		let score = 0;
+		for (let i = 0; i < this.frames.length; i++) {
+			const frame = this.frames[i];
+			let spare = false;
+			if (i !== 0) {
+				const prevFrame = this.frames[i - 1];
+				spare = prevFrame.length === 2 && prevFrame[0] + prevFrame[1] === 10;
+				score += frame[0] ?? 0;
+			}
+			for (let j = 0; j < frame.length; j++) {
+				const pins = frame[j];
+				score += pins;
+			}
+		}
+		return score;
+	}
 }
 
 module.exports = Bowling;
--- index.test.js
+++ index.test.js
@@ -1,9 +1,40 @@
 const Bowling = require("./index");
 
 test("should get 0 points after 0 pins hit on a roll", () => {
-  const bowling = new Bowling();
+	const bowling = new Bowling();
 
-  bowling.roll(0);
+	bowling.roll(0);
 
-  expect(bowling.getScore()).toBe(3);
+	expect(bowling.getScore()).toBe(0);
+});
+
+test("should get 1 points after 1 pin hit on a roll", () => {
+	const bowling = new Bowling();
+
+	bowling.roll(1);
+
+	expect(bowling.getScore()).toBe(1);
+});
+
+test("should get 2 points after 2 pins hit on 2 rolls", () => {
+	const bowling = new Bowling();
+
+	bowling.roll(1);
+	bowling.roll(1);
+
+	expect(bowling.getScore()).toBe(2);
+});
+
+test("should add next roll onto previous frame after spare", () => {
+	const bowling = new Bowling();
+
+	// Frame 1
+	bowling.roll(6);
+	bowling.roll(4);
+
+	// Frame 2
+	bowling.roll(5);
+	bowling.roll(1);
+
+	expect(bowling.getScore()).toBe(21);
 });
